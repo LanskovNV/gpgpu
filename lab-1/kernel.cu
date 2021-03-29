1c6cefc7047d8bb025b20fb59ae8c4c0bf410fee
LanskovNV <leins275@gmail.com>
2021-03-29 09:57:07 +0300

    Updated:  - separated matrixRelease function  - created custom exception class for errors handling

--- lab-1/kernel.cu
+++ lab-1/kernel.cu
@@ -1,12 +1,11 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <stdio.h>
-
 // #include <vld.h>
 #include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <exception>
 #include <random>
 
 typedef double type_t;
@@ -15,9 +14,62 @@ const size_t size = 2000;
 const size_t blockSize = 32;
 
 type_t* matrixCreate();
+void matrixRelease(type_t *matrix);
 type_t maxDiff(type_t* a, type_t* b);
 float multiplyWithCPU(type_t* a, type_t* b, type_t* c);
 float multiplyWithCuda(type_t* a, type_t* b, type_t* c);
+
+namespace MyCudaErrorHandler
+{
+    enum errorCodes
+    {
+        CUDA_START,
+        CUDA_MALLOC,
+        CUDA_MEMCPY,
+        CUDA_LAUNCH_KERNEL,
+        CUDA_DEVICE_SYNCHRONIZE,
+    };
+
+    class MyCudaException : public std::exception
+    {
+        cudaError_t status;
+        errorCodes err;
+    public:
+        MyCudaException(errorCodes errorType, cudaError_t& cudaStatus) : err(errorType), status(cudaStatus) {};
+
+        void printInfo()
+        {
+            switch (err)
+            {
+            case CUDA_START:
+                std::cout << "\ncudaSetDevice failed!\n Do you have a CUDA-capable GPU installed?\n";
+                break;
+            case CUDA_MALLOC:
+                std::cout << "cudaMalloc failed!";
+                break;
+            case CUDA_MEMCPY:
+                std::cout << "cudaMemcpy failed!";
+                break;
+            case CUDA_LAUNCH_KERNEL:
+                std::cout << "addKernel launch failed: " << cudaGetErrorString(status) << std::endl;
+                break;
+            case CUDA_DEVICE_SYNCHRONIZE:
+                std::cout << "cudaDeviceSynchronize returned error code " << status << " after launching addKernel!\n";
+                break;
+            default:
+                std::cout << "Unsupported error type!!!\n" << cudaGetErrorString(status);
+            }
+        }
+    };
+
+    void checkCudaStatus(errorCodes errorType, cudaError_t& cudaStatus)
+    {
+        if (cudaStatus != cudaSuccess)
+            throw MyCudaException(errorType, cudaStatus);
+    }
+}
+
+
 
 __global__ void multiplyKernel(type_t* a, type_t* b, type_t* c)
 {
@@ -42,20 +94,18 @@ int main()
     type_t* b = matrixCreate();
     type_t* cp = matrixCreate();
     type_t* gp = matrixCreate();
-    
+ 
     std::cout << "Started, matrix size - " << size << ", gpu block size: " << blockSize << std::endl;
-
     float gpuTime = multiplyWithCuda(a, b, gp);
     std::cout << "GPU elapsed time (in seconds): " << gpuTime << std::endl;
     float cpuTime = multiplyWithCPU(a, b, cp);
     std::cout << "CPU elapsed time (in seconds): " << cpuTime << std::endl;
-    
     std::cout << "Max diff: " << maxDiff(cp, gp) << std::endl;
 
-    delete[] a;
-    delete[] b;
-    delete[] cp;
-    delete[] gp;
+    matrixRelease(a);
+    matrixRelease(b);
+    matrixRelease(cp);
+    matrixRelease(gp);
     return 0;
 }
 
@@ -90,6 +140,11 @@ type_t* matrixCreate()
     }
 
     return matrix;
+}
+
+void matrixRelease(type_t* matrix)
+{
+    delete[] matrix;
 }
 
 float multiplyWithCPU(type_t* a, type_t* b, type_t* c)
@@ -127,77 +182,64 @@ float multiplyWithCuda(type_t *a, type_t *b, type_t *c)
     dim3 grid(gridDim, gridDim);
 
     cudaError_t cudaStatus;
-
     cudaEvent_t start, stop;
+    
     float gpuTime = 0.0f;
 
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-
     cudaEventRecord(start, 0);
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        std::cout << stderr << "\ncudaSetDevice failed!\n Do you have a CUDA-capable GPU installed?\n";
-        goto Error;
-    }
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, byteSize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-    cudaStatus = cudaMalloc((void**)&dev_a, byteSize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-    cudaStatus = cudaMalloc((void**)&dev_b, byteSize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+    try 
+    {
+        cudaStatus = cudaSetDevice(0);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_START, cudaStatus);
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, byteSize, cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
+        // Allocate GPU buffers for three vectors (two input, one output)    .
+        cudaStatus = cudaMalloc((void**)&dev_c, byteSize);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_MALLOC, cudaStatus);
+      
+        cudaStatus = cudaMalloc((void**)&dev_a, byteSize);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_MALLOC, cudaStatus);
+
+        cudaStatus = cudaMalloc((void**)&dev_b, byteSize);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_MALLOC, cudaStatus);
+
+        // Copy input vectors from host memory to GPU buffers.
+        cudaStatus = cudaMemcpy(dev_a, a, byteSize, cudaMemcpyHostToDevice);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_MEMCPY, cudaStatus);
+
+        cudaStatus = cudaMemcpy(dev_b, b, byteSize, cudaMemcpyHostToDevice);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_MEMCPY, cudaStatus);
+
+        // Launch a kernel on the GPU with one thread for each element.
+        multiplyKernel <<<grid, block >>> (dev_a, dev_b, dev_c);
+
+        // Check for any errors launching the kernel
+        cudaStatus = cudaGetLastError();
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_LAUNCH_KERNEL, cudaStatus);
+
+        // cudaDeviceSynchronize waits for the kernel to finish, and returns
+        // any errors encountered during the launch.
+        cudaStatus = cudaDeviceSynchronize();
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_DEVICE_SYNCHRONIZE, cudaStatus);
+
+        // Copy output vector from GPU buffer to host memory.
+        cudaStatus = cudaMemcpy(c, dev_c, byteSize, cudaMemcpyDeviceToHost);
+        MyCudaErrorHandler::checkCudaStatus(MyCudaErrorHandler::CUDA_MEMCPY, cudaStatus);
     }
-    cudaStatus = cudaMemcpy(dev_b, b, byteSize, cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
+    catch (MyCudaErrorHandler::MyCudaException &e)
+    {
+        e.printInfo();
+
+        cudaFree(dev_a);
+        cudaFree(dev_b);
+        cudaFree(dev_c);
+        cudaEventRecord(stop, 0);
+
+        return -1;
     }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    multiplyKernel<<<grid, block>>>(dev_a, dev_b, dev_c);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, byteSize, cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-
-Error:
+    
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
